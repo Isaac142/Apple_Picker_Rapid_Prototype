fdf2f4b834baa7750cc7421f836c8fc88f03b9c9
Isaac <embramaze@gmail.com>
2019-10-02 19:29:05 +1000

    UI Menus

---
+++ Apple Picker Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //Creating a GameManager to hold all the variabled and functions that are specific to the game.
+    public bool isPaused = false;
+    public UIManager UI;
+
+    void Start()
+    {
+        
+    }
+
+
+    //Creating the Pause Screen, if the pause screen is activated, the timer on the game, will freeze, along with all other UI.
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            UI.Pause(isPaused);
+        }
+    }
+}
